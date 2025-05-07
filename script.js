bb1d86576dd88a1744290da8d576099a485ec143
emiliano-chp-dev <echp.dev@gmail.com>
2025-05-07 16:34:00 +0300

    Import DOM elements and store them in variables

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 'use strict';
 
 function main() {
+  // DOM elements
+  const [previousOperandElement, currentOperandElement] = [
+    ...document.querySelectorAll('.__display-element'),
+  ];
+  const [clearBtn, deleteBtn, signBtn, equalsBtn] = [
+    ...document.querySelectorAll('.__action'),
+  ];
+  const operands = [...document.querySelectorAll('.operand')];
+  const operators = [...document.querySelectorAll('.operator')];
+
   // ...
 }
 
